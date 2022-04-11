39e25f09d67dca7aca90fbe1f85393a7f5726b48
felipe-hgc <101941503+felipe-hgc@users.noreply.github.com>
2022-04-11 16:53:50 -0400

    Ajustes de estilos y tooltips

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
-window.onscroll = function() {scrollFunction()};
+/* Inicializa los tooltips usados en íconos rrss */
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+
+});
+
+/*Llama a la función scrollFunction que chequea altura de carrousel al hacer scroll*/
+window.onscroll = function () {
+  scrollFunction();
+};
+
+/* Chequea altura del carrousel y aplica estilos a la navbar */
 function scrollFunction() {
-  let altura=$('.carousel').height();
+  let altura = $(".carousel").height();
   console.log(altura);
-  if (document.body.scrollTop > altura || document.documentElement.scrollTop > altura) {
-    
+  if (
+    document.body.scrollTop > altura ||
+    document.documentElement.scrollTop > altura
+  ) {
     document.getElementById("navbar").style.background = "#007bff";
   } else {
-   
     document.getElementById("navbar").style.background = "none";
   }
 }
